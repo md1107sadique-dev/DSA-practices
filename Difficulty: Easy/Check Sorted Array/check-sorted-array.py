d36e2387447bb3aaf45ba563391c859dd45704c7
Md Sadique <md1107sadique@gmail.com>
2026-09-01 08:36:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Sorted Array/check-sorted-array.py
+++ Difficulty: Easy/Check Sorted Array/check-sorted-array.py
@@ -1,7 +1,8 @@
 class Solution:
     def isSorted(self, arr):
-        for i in range(0,len(arr)-2):
-            if arr[i]>arr[i+1]:
+        n = len(arr)
+        for i in range(1,n-1):
+            if arr[i-1]> arr[i]:
                 return False
         return True
         
